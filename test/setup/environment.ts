7121390b8c0d20c4fb8d3ed9d32b885b44a8433d
jjfive <ohlab.kr@gmail.com>
2026-09-03 03:57:14 +0900

    fix: 테스트 환경 MessageChannel 포트 누수로 인한 forceExit 의존성 제거
    resetModules로 테스트 파일마다 scheduler가 새 MessageChannel을
    생성하지만 아무도 닫지 않아 worker_threads 핸들이 누적되어
    Jest가 --forceExit 없이는 종료되지 못했다. 환경 인스턴스가 생성한
    포트를 추적해 teardown 시점에 닫도록 수정. React 18/19 전체
    스위트를 forceExit 없이 재검증(240 suites / 1316 passed / 1 skipped).

--- test/setup/environment.ts
+++ test/setup/environment.ts
@@ -4,12 +4,17 @@ import type {
 } from '@jest/environment';
 import JSDOMEnvironment from 'jest-environment-jsdom';
 import { TextDecoder, TextEncoder } from 'util';
-import { MessageChannel } from 'worker_threads';
+import { MessageChannel, type MessagePort } from 'worker_threads';
 
 import attachRafStub from './attach-raf-stub';
 import transitionEventPolyfill from './transition-event-polyfill';
 
 export default class MyJSDOMEnvironment extends JSDOMEnvironment {
+  // resetModules: true 설정 때문에 테스트 파일마다 scheduler가 새 MessageChannel을
+  // 생성한다. 이 환경 인스턴스가 만든 포트를 추적해 teardown 시점에 닫지 않으면
+  // worker_threads의 열린 핸들이 프로세스 종료를 막아 --forceExit이 필요해진다.
+  private readonly openMessagePorts = new Set<MessagePort>();
+
   constructor(config: JestEnvironmentConfig, context: EnvironmentContext) {
     super(config, context);
 
@@ -22,8 +27,26 @@ export default class MyJSDOMEnvironment extends JSDOMEnvironment {
     this.global.TextEncoder =
       TextEncoder as unknown as typeof this.global.TextEncoder;
 
+    const openMessagePorts = this.openMessagePorts;
+    class TrackedMessageChannel extends MessageChannel {
+      constructor() {
+        super();
+        openMessagePorts.add(this.port1);
+        openMessagePorts.add(this.port2);
+      }
+    }
+
     // FIXME: There's some types issues here
     this.global.MessageChannel =
-      MessageChannel as unknown as (typeof this.global)['MessageChannel'];
+      TrackedMessageChannel as unknown as (typeof this.global)['MessageChannel'];
+  }
+
+  async teardown(): Promise<void> {
+    for (const port of this.openMessagePorts) {
+      port.close();
+    }
+    this.openMessagePorts.clear();
+
+    await super.teardown();
   }
 }
